f52082343ce5c6e3ab59b9fc0f596cd9b262c088
pathgate <79215397+xmas-ar@users.noreply.github.com>
2025-05-23 19:14:28 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,15 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../backend'))
+
+
+# -- EPUB settings -----------------------------------------------------
+epub_title = "vMark Documentation"
+epub_author = "Pathgate"
+epub_publisher = "Pathgate"
+epub_copyright = "2025, Pathgate"
+
+# -- Project information ------------------------------------------------
+project = 'vMark'
+author = 'Pathgate'
+version = '0.1.5'
